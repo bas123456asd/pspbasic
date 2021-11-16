f4b06f2b9d868f380d2c69a8ee7096881508bd1e
bas123456asd <93953809+bas123456asd@users.noreply.github.com>
2021-11-16 11:06:10 +0700

    variable
    16/11/64

---
+++ variable.php
@@ -0,0 +1,73 @@
+<?php
+$a = "hello";
+echo($a);
+echo("<br>")
+default("[PROVINCE","Bangkok")
+echo PROVINCE;
+echo("<br>")
+default("SALARY",15000)
+echo SALARY;
+echo("<br>")
+$t = date("H");
+echo $t;
+echo("<br>")
+if ($t < "20") {
+    echo " Have a good day!";
+}else {echo "Have a  night!";
+
+}
+
+f ($t < "10") {
+    echo " Have a good morning!";
+}elseif ($t < "20")
+{echo "Have a  good day!";
+} else {
+    echo "Have a good night!";
+}
+
+
+$favcolor = "red";
+
+switch ($favcolor) {
+ case "red": 
+    echo "Your favcorite  color is red!";
+    break;
+ case "blue":
+    echo "Your favcorite color is blue!";
+    break;
+ case "green":
+    echo "Tour favcorite color green!";
+    break;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <header
+          <l -- Required meta tags  == >
+          <meta charset = "utf-8">
+          <meta name = "viewport" content = 
+          <title></title>
+    </header>
+    <body>
+        <h1>
+         <?php
+         echo($a);
+         ?>  
+        </h1>
+        <div style = "font - size : larger ; color:blue;">
+        <?php
+             echo"<b>คณะบริหารฑุรกิจ สาขาเทตโนโลยัสารสนเทศทางธุรกิจ
+             ประกอบด้วย 3 วิชาเอก</b>";
+        ?>
+    </div>
+    <ul>
+        <?php
+            echo"<li>การจัดการ</li>";
+            echo"<li>พีฒนาซอร์ฟเเวร์</li>";
+            echo"<li>มิลติมีเดีย</li>";
+            ?>
+        </ul>        
+   
+    </body>
+</html>
