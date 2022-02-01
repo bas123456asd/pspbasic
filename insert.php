009298e6ced171306e66fb4343f586f4f5cdac90
bas123456asd <93953809+bas123456asd@users.noreply.github.com>
2022-02-01 13:31:13 +0700

    1

---
+++ insert.php
@@ -0,0 +1,12 @@
+<?php
+require ('connect.php');
+
+$EmployeeID = $_REQUEST['EmployeeID'];
+$Title = $_REQUEST['Title'];
+$Name = $_REQUEST['Name'];
+$Sex = $_REQUEST['Sex'];
+$Education = $_REQUEST['Education'];
+$Start_Date = $_REQUEST['Start_Date'];
+$Salary = $_REQUEST['Salary'];
+$DepartmentID = $_REQUEST['DepartmentID'];
+?>
